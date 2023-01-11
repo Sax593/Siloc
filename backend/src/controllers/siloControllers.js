d6d9dcf7611d6c873b0c88a393475bace8459bef
sax59 <waquetv@gmail.com>
2023-01-11 14:57:07 +0100

    Add all route

---
+++ backend/src/controllers/siloControllers.js
@@ -0,0 +1,91 @@
+const models = require("../models");
+
+const browse = (req, res) => {
+  models.silo
+    .findAll()
+    .then(([rows]) => {
+      res.send(rows);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const read = (req, res) => {
+  models.silo
+    .find(req.params.id)
+    .then(([rows]) => {
+      if (rows[0] == null) {
+        res.sendStatus(404);
+      } else {
+        res.send(rows[0]);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const edit = (req, res) => {
+  const silo = req.body;
+
+  // TODO validations (length, format...)
+
+  silo.id = parseInt(req.params.id, 10);
+
+  models.silo
+    .update(silo)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const add = (req, res) => {
+  const silo = req.body;
+
+  // TODO validations (length, format...)
+
+  models.silo
+    .insert(silo)
+    .then(([result]) => {
+      res.location(`/silos/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+const destroy = (req, res) => {
+  models.silo
+    .delete(req.params.id)
+    .then(([result]) => {
+      if (result.affectedRows === 0) {
+        res.sendStatus(404);
+      } else {
+        res.sendStatus(204);
+      }
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+};
+
+module.exports = {
+  browse,
+  read,
+  edit,
+  add,
+  destroy,
+};
