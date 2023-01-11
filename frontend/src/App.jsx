d680fe6200af621aa5cfcac4d213849db19a7360
Alexis Foulont <alexisfoulont@gmail.com>
2023-01-11 17:09:31 +0100

    modified routes into browserRouter + carImage add

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
-import Home from "@pages/Home/Home";
 import { Route, Routes } from "react-router-dom";
+import CarImage from "@components/CarImage/CarImage";
+// import Home from "@pages/Home/Home";
 
 import "./App.scss";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
+      {/* <Route path="/" element={<Home />} /> */}
+      <Route path="/" element={<CarImage />} />
     </Routes>
   );
 }
--- frontend/src/components/CarImage/CarImage.jsx
+++ frontend/src/components/CarImage/CarImage.jsx
@@ -1,10 +1,10 @@
 import "./style.scss";
-import { car1 } from "../../assets/cars_image/car1.png";
+import car1 from "../../assets/cars_image/car1.png";
 
 export default function CarImage() {
   return (
-    <div className="carImage">
-      <img src={car1} alt="ElectricCar" />
-    </div>
+    <section className="carImage">
+      <img className="electricCar1" src={car1} alt="ElectricCar" />
+    </section>
   );
 }
