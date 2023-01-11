7f350bceba3ba86d0d21615ce8699be7d101cfee
Alexis Foulont <alexisfoulont@gmail.com>
2023-01-11 18:00:29 +0100

    assemble components into page + responsive design

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "@pages/Home/Home";
-
+import SingleCarScreen from "@pages/SingleCarScreen/SingleCarScreen";
 import "./App.scss";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/car" element={<SingleCarScreen />} />
     </Routes>
   );
 }
--- frontend/src/pages/SingleCarScreen/SingleCarScreen.jsx
+++ frontend/src/pages/SingleCarScreen/SingleCarScreen.jsx
@@ -1,5 +1,16 @@
+import CarImage from "@components/CarImage/CarImage";
+import CarInfo from "@components/CarInfo/CarInfo";
 import "./style.scss";
 
 export default function SingleCarScreen() {
-  return <div />;
+  return (
+    <div className="carPage">
+      <section className="carOverview">
+        <CarImage />
+      </section>
+      <section className="carInfoOverview">
+        <CarInfo />
+      </section>
+    </div>
+  );
 }
