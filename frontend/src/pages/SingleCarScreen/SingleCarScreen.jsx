ef3fd58be343aefea6d87693802b7604895b0ad9
Alexis Foulont <alexisfoulont@gmail.com>
2023-01-11 20:38:43 +0100

    add rating star

--- frontend/src/pages/SingleCarScreen/SingleCarScreen.jsx
+++ frontend/src/pages/SingleCarScreen/SingleCarScreen.jsx
@@ -1,5 +1,6 @@
 import CarImage from "@components/CarImage/CarImage";
 import CarInfo from "@components/CarInfo/CarInfo";
+import { AiFillStar } from "react-icons/ai";
 import "./style.scss";
 
 export default function SingleCarScreen() {
@@ -8,6 +9,25 @@ export default function SingleCarScreen() {
       <section className="carOverview">
         <CarImage />
       </section>
+      <div className="rating">
+        <div className="stars">
+          <i className="fa fa-star gold">
+            <AiFillStar />
+          </i>
+          <i className="fa fa-star gold">
+            <AiFillStar />
+          </i>
+          <i className="fa fa-star">
+            <AiFillStar />
+          </i>
+          <i className="fa fa-star">
+            <AiFillStar />
+          </i>
+          <i className="fa fa-star">
+            <AiFillStar />
+          </i>
+        </div>
+      </div>
       <section className="carInfoOverview">
         <CarInfo />
       </section>
