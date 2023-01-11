19b6a94da36f283d4cf67dbe57d1090b93f1401c
Fbouchezpro <fbouchezpro@gmail.com>
2023-01-11 22:59:17 +0100

    final changes

--- frontend/src/components/Loginform/LoginForm.jsx
+++ frontend/src/components/Loginform/LoginForm.jsx
@@ -1,12 +1,25 @@
-import "./Style.scss";
+import "./style.scss";
 
 export default function LoginForm() {
   return (
     <div className="lgForm">
-      <p>je suis ici</p>
-      <input type="text" placeholder="Email" id="username" />
-      <input type="password" placeholder="Password" id="password" />
-      <button type="button">Log In!</button>
+      <h4 className="loginh4">E-mail: </h4>
+      <input
+        className="logininp"
+        type="text"
+        placeholder="Email"
+        id="username"
+      />
+      <h4 className="loginh4">Password:</h4>
+      <input
+        className="logininp"
+        type="password"
+        placeholder="Password"
+        id="password"
+      />
+      <button className="loginbtn" type="button">
+        Log In!
+      </button>
     </div>
   );
 }
