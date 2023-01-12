4578d49662e10bcc065c4446285be54421216227
Priscillia <selvapriscillia@yahoo.fr>
2023-01-12 11:51:43 +0100

    create page car transfert and style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,9 @@ import Checkout from "@pages/Checkout/Checkout";
 import { Route, Routes } from "react-router-dom";
 import SingleCarScreen from "@pages/SingleCarScreen/SingleCarScreen";
 import CarDashboardBySilo from "@pages/CarDashboardBySilo/CarDashboardBySilo";
-import "./App.scss";
-
 import Form from "@pages/Form/Form";
+import CarTransfert from "@pages/CarTransfert/CarTransfert";
+import "./App.scss";
 
 function App() {
   return (
@@ -15,6 +15,7 @@ function App() {
       <Route path="/bookingScreen" element={<BookingScreen />} />
       <Route path="/form" element={<Form />} />
       <Route path="/car" element={<SingleCarScreen />} />
+      <Route path="/carTransfert" element={<CarTransfert />} />
       <Route path="/dashboard" element={<CarDashboardBySilo />} />
       <Route path="/checkout" element={<Checkout />} />
     </Routes>
