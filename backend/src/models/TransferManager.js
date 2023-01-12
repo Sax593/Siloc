7d27b28b3a49a3c3fb5836cf6789d434af94b7ae
sax59 <waquetv@gmail.com>
2023-01-12 11:18:27 +0100

    Fix transfermanager

--- backend/src/models/TransferManager.js
+++ backend/src/models/TransferManager.js
@@ -7,28 +7,15 @@ class transferManager extends AbstractManager {
 
   insert(transfer) {
     return this.connection.query(
-      `insert into ${this.table} (vehicle_type, departure_time, arrival_time, silo_id, users_id) values (?,?,?,?,?)`,
-      [
-        transfer.vehicle_type,
-        transfer.departure_time,
-        transfer.arrival_time,
-        transfer.silo_id,
-        transfer.users_id,
-      ]
+      `insert into ${this.table} (nb_vehicles, time, silo_id) values (?,?,?)`,
+      [transfer.nb_vehicles, transfer.time, transfer.silo_id]
     );
   }
 
   update(transfer) {
     return this.connection.query(
-      `update ${this.table} set vehicle_type = ?, departure_time = ?, arrival_time = ?, silo_id = ?, users_id = ? where id = ?`,
-      [
-        transfer.vehicle_type,
-        transfer.departure_time,
-        transfer.arrival_time,
-        transfer.silo_id,
-        transfer.users_id,
-        transfer.id,
-      ]
+      `update ${this.table} set nb_vehicles = ?, time = ?, silo_id = ? where id = ?`,
+      [transfer.nb_vehicles, transfer.time, transfer.silo_id, transfer.id]
     );
   }
 }
