0d748cd3feb7fb36ddeaa9ae43b0bbdb10ed0738
sax59 <waquetv@gmail.com>
2023-01-12 18:33:29 +0100

    Add back link

--- frontend/src/components/ListingCar/ListingCar.jsx
+++ frontend/src/components/ListingCar/ListingCar.jsx
@@ -1,15 +1,29 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
 import "./style.scss";
 
 export default function ListingCar() {
+  const [listing, setListing] = useState([]);
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_BACKEND_URL}/vehiculesfind/1`)
+      .then(({ data }) => {
+        setListing(data);
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }, []);
   return (
     <article className="vehiclelisting">
       <ul>
-        <li className="car">Car 1</li>
-        <li className="car">Car 1</li>
-        <li className="car">Car 1</li>
-        <li className="car">Car 1</li>
-        <li className="car">Car 1</li>
-        <li className="car">Car 1</li>
+        {listing.map((element) => {
+          return (
+            <li className="car" key={element.id}>
+              {element.brand} {element.type} {element.mileage}miles
+            </li>
+          );
+        })}
       </ul>
     </article>
   );
