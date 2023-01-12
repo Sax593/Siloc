a222aaa6a29e9cb418c5ad672d3318338a6451fc
Fbouchezpro <fbouchezpro@gmail.com>
2023-01-12 11:43:40 +0100

    adding final changes on homepage

--- frontend/src/components/Loginform/LoginForm.jsx
+++ frontend/src/components/Loginform/LoginForm.jsx
@@ -1,13 +1,69 @@
+// import axios from "axios";
+import { useState } from "react";
 import "./style.scss";
 
 export default function LoginForm() {
+  const [formData, setFormData] = useState({
+    name: "",
+    password: "",
+  });
+
+  // const inputRef = useRef();
+
+  const hChange = (evt) => {
+    const { name, value, type, checked } = evt.target;
+    let newValue = null;
+    switch (type) {
+      case "checkbox":
+        newValue = checked;
+        break;
+      case "file":
+        return;
+      default:
+        newValue = value;
+    }
+    setFormData({ ...formData, [name]: newValue });
+  };
+  // const hSubmit = (evt) => {
+  //   evt.preventDefault();
+
+  //   axios
+  //     .post("/auth/login", formData)
+  //     .then(({ data }) => {
+  //       const { token, user } = data;
+  //       axios.defaults.headers.authorization = `Bearer ${token}`;
+  //       dispatch({ type: "USER_LOGIN", payload: { ...user, token } });
+  //       console.warn(
+  //         "TODO !!! Remove token from Redux and implement httpOnly cookie !"
+  //       );
+  //       send.status(201);
+  //     })
+  //     .catch(() => {
+  //       send.status(500);
+  //     });
+  // };
   return (
     <div className="lgForm">
+      <legend className="loginh4">Log in your next trip over city</legend>
       <h4 className="loginh4">E-mail: </h4>
-      <input className="logininp" type="text" id="username" />
+      <input
+        className="logininp"
+        type="text"
+        id="username"
+        name="name"
+        value={formData.name}
+        onChange={hChange}
+      />
       <h4 className="loginh4">Password:</h4>
-      <input className="logininp" type="password" id="password" />
-      <button className="loginbtn" type="button">
+      <input
+        className="logininp"
+        type="password"
+        id="password"
+        name="password"
+        value={formData.password}
+        onChange={hChange}
+      />
+      <button className="loginbtn" type="submit">
         Log In!
       </button>
       <a className="loginh4" href="/form">
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,11 +1,12 @@
 import LoginForm from "@components/Loginform/LoginForm";
 import Logo from "@components/Logo/Logo";
+import "./style.scss";
 
 export default function Home() {
   return (
-    <>
+    <div className="homepage">
       <Logo />
       <LoginForm />
-    </>
+    </div>
   );
 }
