a7b8e70fdbae22a47d25e6bd22d3aaec301a173b
Sax59 <waquetv@gmail.com>
2023-01-13 00:32:43 +0100

    Fix bad issue for booking query

--- frontend/src/pages/BookingScreen/BookingScreen.jsx
+++ frontend/src/pages/BookingScreen/BookingScreen.jsx
@@ -1,23 +1,15 @@
 import "./style.scss";
 import DateTime from "@components/DateTime/DateTime";
 import logo from "@assets/logo.png";
-import { useState } from "react";
 
 export default function BookingScreen() {
-  const [data, setData] = useState({
-    vehicle_type: "",
-    departure_date: "",
-    arrival_date: "",
-    silo_id: "",
-  });
-
   return (
     <>
       <h1 className="titleBooking">Book a car</h1>
       <section className="carImage">
         <img className="electricCar1" src={logo} alt="ElectricCar" />
       </section>
-      <DateTime data={data} setData={setData} />
+      <DateTime />
     </>
   );
 }
--- frontend/src/pages/BookingScreen/BookingScreen1.jsx
+++ frontend/src/pages/BookingScreen/BookingScreen1.jsx
@@ -1,23 +1,15 @@
 import "./style.scss";
 import DateTime from "@components/DateTime/DateTime";
-import { useState } from "react";
 import car1 from "@assets/cars_image/car1.png";
 
 export default function BookingScreen() {
-  const [data, setData] = useState({
-    vehicle_type: "",
-    departure_date: "",
-    arrival_date: "",
-    silo_id: "",
-  });
-
   return (
     <>
       <h1 className="titleBooking">Book a car</h1>
       <section className="carImage">
         <img className="electricCar1" src={car1} alt="ElectricCar" />
       </section>
-      <DateTime data={data} setData={setData} />
+      <DateTime />
     </>
   );
 }
