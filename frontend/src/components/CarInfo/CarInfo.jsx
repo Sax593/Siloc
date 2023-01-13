5d985f14ca9784de95d18847f1fb1847da641da4
sax59 <waquetv@gmail.com>
2023-01-13 09:44:22 +0100

    Fix date format

--- frontend/src/components/CarInfo/CarInfo.jsx
+++ frontend/src/components/CarInfo/CarInfo.jsx
@@ -2,8 +2,10 @@ import { useEffect, useState } from "react";
 import "./style.scss";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import { DateTime } from "luxon";
 
 export default function CarInfo() {
+  const format = "dd/MM/yy HH:mm:ss";
   const [vehicle, setVehicle] = useState([]);
   const [status, setStatus] = useState("unavailable");
   const { id } = useParams();
@@ -31,7 +33,11 @@ export default function CarInfo() {
       <div className="kilometers" key={vehicle.id}>
         {vehicle.mileage} km
       </div>
-      <div className="lastRent">Last rent: {vehicle.lastrent}</div>
+      <div className="lastRent">
+        Last rent:{" "}
+        {vehicle.lastrent &&
+          DateTime.fromISO(vehicle.lastrent).toFormat(format)}
+      </div>
       <div className="kmRemaining">
         {10000 - vehicle.mileage} Miles remaining before maintenance
       </div>
